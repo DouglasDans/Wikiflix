e91146a31f1cdabbbd2a26a37c1bc5ae5b58e5f9
DouglasDans <douglas.dans@gmail.com>
2022-08-16 23:21:46 -0300

    APIrequests.js Criado

---
+++ src/APIrequests.js
@@ -0,0 +1,9 @@
+
+const API_URL = "https://api.themoviedb.org/3";
+const API_KEY = "api_key=2b22702bf9dc45986d22dd21add08ec7"
+const API_LANGUAGE = "language=pt-BR"
+
+
+export default function fetchPopularGames(){
+    fetch(API_URL + '/movie/popular?'+ API_KEY +'&'+ API_LANGUAGE +'&page=1').then(response => response.json()).then(data => console.log(data))
+}
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
+import fetchPopularGames from "../../APIrequests";
 import Carousel from "../Carousel";
 import Navbar from "../Navbar";
 import SearchContainer from "../SearchContainer";
@@ -10,7 +11,7 @@ function Home(){
     //useEffect - Serve para lidar com os efeitos colaterais de usar dados externos como uma api
 
     useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=2b22702bf9dc45986d22dd21add08ec7&language=pr-BR&page=1').then(response => response.json()).then(data => console.log(data))
+        fetchPopularGames()
     },[]);
 
     return(
@@ -18,13 +19,16 @@ function Home(){
             <Navbar />
             <main className="container-main">
                 <SearchContainer/>
-                <Carousel/>
+                <div className="home-container">
+                    <Carousel/>
+                    {movies.map(movie => {
+                        return(
+                            <div>ss</div>
+                        )
+                    })}
+                </div>
 
-                {movies.map(movie => {
-                    return(
-                        <div>ss</div>
-                    )
-                })}
+                
             </main>
         </Fragment>
     )
