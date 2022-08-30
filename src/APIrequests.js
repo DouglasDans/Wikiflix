0dac4b29b1e9237e811ebcae05067dde28b09222
DouglasDans <douglas.dans@gmail.com>
2022-08-29 21:40:17 -0300

    Carousels foram separados em dois components

--- src/APIrequests.js
+++ src/APIrequests.js
@@ -1,11 +1,13 @@
 
+
 const API_URL = "https://api.themoviedb.org/3";
 const API_KEY = "api_key=2b22702bf9dc45986d22dd21add08ec7"
 const API_LANGUAGE = "language=pt-BR"
 
 
-export default function fetchPopularGames(){
-    let list = ""
-    fetch(API_URL + '/movie/popular?'+ API_KEY +'&'+ API_LANGUAGE +'&page=1').then(response => response.json()).then(list => list)
-    return list
+export default function getPopularGames(){
+    fetch(API_URL + '/movie/popular?'+ API_KEY +'&'+ API_LANGUAGE +'&page=2').then(response => response.json()).then(data => {
+        console.log(data.results)
+        return data.results
+    })
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,44 +6,109 @@ import './Carousel.css'
 
 export default function Carousel(props) {
     console.log(props.itens);
+
+    if(props.coverType == 'large'){
+        return(
+            <CarouselLarge itens={props.itens} title={props.title}/>
+        )
+    }else{
+        return(
+            <CarouselSmall itens={props.itens} title={props.title}/>
+        )
+    }
+
     
+    // const itens = props.itens.map((item, index) => {
+    //     if(props.coverType == 'large'){
+    //         const style = { width: 400 };
+    //         return (
+    //             <div className="item" style={style}>
+    //                 <div className='carousel-item-container'>
+    //                     <div className='title-carousel-item'>
+    //                         <h1>{item.title}</h1>
+    //                         <span>{item.release_date}</span>
+    //                     </div>
+    //                     <img className='carousel-img' height="215px" width="380px" src={'https://image.tmdb.org/t/p/w500/' + item.backdrop_path} />
+    //                 </div>
+    //             </div>
+    //         );
+    //     } else{
+    //         const style = { width: 130 };
+    //         return (
+    //             <div className="item" style={style}>
+    //                     <img className='carousel-img' height="165px" width="110px" src={'https://image.tmdb.org/t/p/w500/' + item.poster_path} />
+    //             </div>
+    //         );
+    //     } 
+    // });
+    // return (
+    //     <div className="carousel-container">
+    //         <span className='carousel-title'>{props.title}</span>
+    //         <AliceCarousel
+    //             items={itens}
+    //             autoWidth  
+    //             mouseTracking
+    //             disableDotsControls
+    //             touchTracking
+    //             // infinite
+    //             paddingLeft={55}
+    //             renderPrevButton={() => {
+    //                 return(
+    //                 <div className='arrow-container left-arrow'>
+    //                     <span class="material-symbols-rounded">
+    //                         chevron_left
+    //                     </span>
+    //                 </div>
+    //                 )
+    //             }}
+    //             renderNextButton={() => {
+    //                 return(
+    //                 <div className='arrow-container right-arrow'>
+    //                     <span class="material-symbols-rounded">
+    //                         chevron_right
+    //                     </span>
+    //                 </div>
+    //                 )
+    //             }}
+                
+    //         />
+    //     </div>
+    // )
+}
+
+function CarouselLarge(props){
     const itens = props.itens.map((item, index) => {
-        if(props.coverType == 'large'){
-            const style = { width: 400 + index };
-            return (
-                <div className="item" style={style}>
+        const style = { width: 400 };
+        return (
+            <div className="item" style={style}>
+                <div className='carousel-item-container'>
+                    <div className='title-carousel-item'>
+                        <h1>{item.title}</h1>
+                        <span>{item.release_date}</span>
+                    </div>
                     <img className='carousel-img' height="215px" width="380px" src={'https://image.tmdb.org/t/p/w500/' + item.backdrop_path} />
-                    
                 </div>
-            );
-        } else{
-            const style = { width: 130 + index };
-            return (
-                <div className="item" style={style}>
-                    <img className='carousel-img' height="165px" width="110px" src={'https://image.tmdb.org/t/p/w500/' + item.poster_path} />
-                    
-                </div>
-            );
-        }
-        
-    });
-
-        
-
+            </div>
+        );
+    })
     return (
         <div className="carousel-container">
             <span className='carousel-title'>{props.title}</span>
             <AliceCarousel
                 items={itens}
-                autoWidth  
+                activeIndex={0}
+                controlsStrategy={"responsive"}
                 mouseTracking
-                disableDotsControls
                 touchTracking
+                touchMoveDefaultEvents
+                autoWidth
+                Height={55}
+                disableDotsControls
                 // infinite
                 paddingLeft={55}
                 renderPrevButton={() => {
                     return(
-                    <div className='arrow-container'>
+                    <div className='arrow-container large-left-arrow'>
                         <span class="material-symbols-rounded">
                             chevron_left
                         </span>
@@ -52,7 +117,7 @@ export default function Carousel(props) {
                 }}
                 renderNextButton={() => {
                     return(
-                    <div className='arrow-container'>
+                    <div className='arrow-container large-right-arrow'>
                         <span class="material-symbols-rounded">
                             chevron_right
                         </span>
@@ -64,23 +129,49 @@ export default function Carousel(props) {
         </div>
     )
 }
-
-function prevButton(){
-    return(
-        <div className='arrow-container'>
-            <span class="material-symbols-rounded">
-                chevron_left
-            </span>
-        </div>
-    )
-}
-
-function nextButton(){
-    return(
-        <div className='arrow-container'>
-            <span class="material-symbols-rounded">
-                chevron_next
-            </span>
+function CarouselSmall(props){
+    const itens = props.itens.map((item, index) => {
+        const style = { width: 130 };
+        return (
+            <div className="item" style={style}>
+                <img className='carousel-img' height="165px" width="110px" src={'https://image.tmdb.org/t/p/w500/' + item.poster_path} />
+            </div>
+        );
+    })
+    return (
+        <div className="carousel-container">
+            <span className='carousel-title'>{props.title}</span>
+            <AliceCarousel
+                items={itens}
+                activeIndex={0}
+                controlsStrategy={"responsive"}
+                mouseTracking
+                touchTracking
+                touchMoveDefaultEvents
+                autoWidth
+                Height={55}
+                disableDotsControls
+                // infinite
+                paddingLeft={55}
+                renderPrevButton={() => {
+                    return(
+                    <div className='arrow-container small-left-arrow'>
+                        <span class="material-symbols-rounded">
+                            chevron_left
+                        </span>
+                    </div>
+                    )
+                }}
+                renderNextButton={() => {
+                    return(
+                    <div className='arrow-container small-right-arrow'>
+                        <span class="material-symbols-rounded">
+                            chevron_right
+                        </span>
+                    </div>
+                    )
+                }}
+            />
         </div>
     )
 }
