dde0aeef11741c4df53640231d3279bd709bb8d4
DouglasDans <douglas.dans@gmail.com>
2023-03-13 19:33:00 -0300

    PWA teste

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,20 @@
-var CACHE_NAME = 'static-v1';
-
-window.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll([
-        '/',
-      ]);
+var cacheName = 'NomeApp-v';
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
-  )
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
