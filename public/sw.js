21a768af6fc17a0d5b2405bfba9ba0c507886d94
DouglasDans <douglas.dans@gmail.com>
2023-03-30 19:19:44 -0300

    sw.js update

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,6 @@
-var cacheName = 'NomeApp-v';
-var filesToCache = [
-    './manifest.json'
-];
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+var cacheName = 'Wikiflix';
+
+
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
