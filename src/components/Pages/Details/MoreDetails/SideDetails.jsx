a76286ffdf78f86b9c04a62571b5d1a7695f7002
douglasdans <douglas.dans@gmail.com>
2024-02-16 21:15:36 -0300

    SideDetails | Correções e adições - Adicionado verificação de existência de dados - Alterado o estilo dos itens - Adicionado novas informações para filmes e para séries

--- src/components/Pages/Details/MoreDetails/SideDetails.jsx
+++ src/components/Pages/Details/MoreDetails/SideDetails.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react'
 
 import './SideDetails.css'
+import convertStringToDate from '../../../../util/convertStringToDate'
 
 export default function SideDetails({details}, typeContent) {
    
-   const releaseDate = new Date(details.release_date || details.first_air_date)
+   const releaseDate = convertStringToDate(details.release_date || details.first_air_date)
 
    const productionCompanies = () =>  {
       let content = ""
@@ -28,32 +29,75 @@ export default function SideDetails({details}, typeContent) {
       return content
    }
 
+   console.log(details)
+
    return (
       <Fragment>
          <div className="info-line">
-            <p className="titulo">Título Original: </p>
+            <span className="titulo">Título Original</span>
             <p>{details.original_title || details.original_name}</p>
          </div>
+
          <div className="info-line">
-            <p className="titulo">Lançamento: </p>
+            <span className="titulo">Lançamento</span>
             <p> {`${releaseDate.getDay()}/${releaseDate.getMonth()}/${releaseDate.getFullYear()}`}</p>
          </div>
+
+         {
+            details.status ? 
+            <div className="info-line">
+               <span className="titulo">Status</span>
+               <p> {`${details.status}`}</p>
+            </div> : <></> 
+         }
+
+         {
+            details.first_air_date ? 
+            <div className="info-line">
+               <span className="titulo">Primeiro episódio exibido</span>
+               <p> {`${releaseDate.getDay()}/${releaseDate.getMonth()}/${releaseDate.getFullYear()}`}</p>
+            </div> : <></> 
+         }
+         
+         {
+            details.last_air_date ? 
+            <div className="info-line">
+               <span className="titulo">Episódio mais recente</span>
+               <p> {`${convertStringToDate(details.last_air_date).getDay()}/${convertStringToDate(details.last_air_date).getMonth()}/${convertStringToDate(details.last_air_date).getFullYear()}`}</p>
+            </div> : <></> 
+         }
+
+         {
+            details.type ? 
+            <div className="info-line">
+               <span className="titulo">Tipo</span>
+               <p> {`${details.type}`}</p>
+            </div> : <></> 
+         }
+         
+         {
+            details.original_language ? 
+            <div className="info-line">
+               <span className="titulo">Idioma Original</span>
+               <p>{details.original_language}</p>
+            </div> : <></> 
+         }
+         
          <div className="info-line">
-            <p className="titulo">Estúdios de Produção: </p>
-            <p>{productionCompanies()}</p>
-         </div>
-         <div className="info-line">
-            <p className="titulo">País de Origem: </p>
+            <span className="titulo">País de Origem</span>
             <p>{productionCountries()}</p>
          </div>
+
          <div className="info-line">
-            <p className="titulo">Idioma Original: </p><p>{details.original_language}</p>
+            <span className="titulo">Estúdios de Produção</span>
+            <p>{productionCompanies()}</p>
          </div>
 
          {
             typeContent === "movie" ? 
             <div className="info-line">
-               <p className="titulo">Duração: </p><p>{`${details.runtime}m`}</p>
+               <span className="titulo">Duração</span>
+               <p>{`${details.runtime}m`}</p>
             </div> : <></> 
          }
          
@@ -61,7 +105,7 @@ export default function SideDetails({details}, typeContent) {
             details.homepage ? 
             <div className="info-line">
                <button className='link-site'>
-                  <a href={details.homepage}><p>Acessar Homepage</p></a>
+                  <a href={details.homepage}><p>Acessar site oficial</p></a>
                </button>
             </div> : <></>
          }
