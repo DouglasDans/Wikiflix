8d4aa38d59f4624b928c3c9d21541c1ea9bd169e
douglasdans <douglas.dans@gmail.com>
2024-02-16 22:29:49 -0300

    Reviews | Corrigido o estilo do componente

--- src/components/Pages/Details/MoreDetails/Review.jsx
+++ src/components/Pages/Details/MoreDetails/Review.jsx
@@ -1,17 +1,22 @@
 import React, { Fragment } from 'react'
+import styles from './review.module.css'
 
 export default function Review({reviews}) {
    try {
        let review = reviews.results[0]
        return (
            <Fragment>
-               <div className="review-container">
-                   <div className="author-image">
-                       <img src={"https://image.tmdb.org/t/p/w500/" + review.author_details.avatar_path}/>
+               <div className={styles.reviewContainer}>
+                   <div className={styles.authorImage}>
+                        {
+                            review.author_details.avatar_path ? 
+                                (<img className={styles.image} src={"https://image.tmdb.org/t/p/w500/" + review.author_details.avatar_path}/>) 
+                                : <span className={"material-symbols-rounded " + styles.placeholder}>account_circle</span>
+                        }
                    </div>
-                   <div className="review-content">
-                       <h4 className="author-name">{review.author}</h4>
-                       <p className="review-text">{review.content}</p>
+                   <div className={styles.reviewContent}>
+                       <h4 className={styles.authorName}>{review.author}</h4>
+                       <p className={styles.reviewText}>{review.content}</p>
                    </div>
                </div>
            </Fragment>
