f313a188b76fcf03fbd85b735355af6686184ebf
douglasdans <douglas.dans@gmail.com>
2024-02-16 21:27:14 -0300

    Descrição | Corrigido estilo dos elementos

--- src/components/Pages/Details/MoreDetails/Descricao.jsx
+++ src/components/Pages/Details/MoreDetails/Descricao.jsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react'
+import styles from './descricao.module.css'
 
 export default function Descricao({details}) {
    try {
       return (
-         <Fragment>
-            <h2>{details.tagline && <>{details.tagline}</>}</h2>
-            <p>{details.overview && <>{details.overview}</>}</p>
-         </Fragment>
+         <div className={styles.descricaoContainer}>
+            <h2 className={styles.tagline}>{details.tagline && <>{details.tagline}</>}</h2>
+            <p className={styles.descricao}>{details.overview && <>{details.overview}</>}</p>
+         </div>
       )
    } catch (error) {
       console.error("[Error - Descricao] " + error);
