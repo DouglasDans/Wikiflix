6d988af0bbd6f9b80fa431cde71c6215d2758cbc
douglasdans <douglas.dans@gmail.com>
2024-02-16 21:51:34 -0300

    YoutubeVideo | Corrigido dimensão do componente

--- src/components/Pages/Details/MoreDetails/YoutubeVideo.jsx
+++ src/components/Pages/Details/MoreDetails/YoutubeVideo.jsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react'
+import style from './youtubeVideo.module.css'
 
 export default function YoutubeVideo({videos}) {
    try {
       let video = videos.results[0]
       return (
          <Fragment>
-            {video && <iframe className="youtube-video" src={`https://www.youtube.com/embed/${video.key}`}title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; fullscreen; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>}
+            {video && <iframe className={style.video} src={`https://www.youtube.com/embed/${video.key}`}title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; fullscreen; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>}
          </Fragment>
       )
    } catch (error) {
